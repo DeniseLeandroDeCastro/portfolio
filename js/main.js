f77a91eeb3b58f8592a5b884b43298a0579a7992
Fernanda Alves <fernanda__m@hotmail.com>
2019-12-07 19:32:29 -0300

    media

--- js/main.js
+++ js/main.js
@@ -66,3 +66,18 @@ function init() {
   // Init TypeWriter
   new TypeWriter(txtElement, words, wait);
 }
+
+// Smooth Scrolling
+$("#navbar a, .btn").on("click", function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      800
+    );
+  }
+});
